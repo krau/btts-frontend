b3d894b55a829e7ec03dcc1a02003d08766d7ae3
krau <71133316+krau@users.noreply.github.com>
2025-07-26 10:20:52 +0800

    feat: add shadcn ui

--- eslint.config.ts
+++ eslint.config.ts
@@ -14,7 +14,7 @@ export default defineConfigWithVueTs(
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/components/ui/**']),
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
